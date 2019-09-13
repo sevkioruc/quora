026af1fedbca02fbffd7a47dea3ef74cad75cc1c
Sevki ORUC <sevki@3bfab.com>
2019-09-13 11:13:40 +0300

    following added

--- quora/user/admin.py
+++ quora/user/admin.py
@@ -1,1 +1,4 @@
 from django.contrib import admin
+from user.models import Follow
+
+admin.site.register(Follow)
--- quora/user/models.py
+++ quora/user/models.py
@@ -1,1 +1,16 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import User
+
+
+class Follow(models.Model):
+    follower = models.ForeignKey(
+        User, related_name='following', on_delete=models.CASCADE)
+    following = models.ForeignKey(
+        User, related_name='followers', on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('follower', 'following')
+
+    def __str__(self):
+        return self.follower.username + ' followed ' + self.following.username
