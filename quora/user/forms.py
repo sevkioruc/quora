3dd9a39ea30d37375c515bcb954240979db1fa3b
Sevki ORUC <sevki@3bfab.com>
2019-09-13 11:09:45 +0300

    Login , Register , Logout added

---
+++ quora/user/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=50, label='Username')
+    password = forms.CharField(
+        max_length=20, label='Password', widget=forms.PasswordInput)
+
+
+class RegisterForm(forms.Form):
+    username = forms.CharField(max_length=50, label='Username')
+    password = forms.CharField(
+        max_length=20, label='Password', widget=forms.PasswordInput)
+    confirm = forms.CharField(
+        max_length=20, label='Password', widget=forms.PasswordInput)
+
+    def clean(self):
+        username = self.cleaned_data.get("username")
+        password = self.cleaned_data.get("password")
+        confirm = self.cleaned_data.get("confirm")
+
+        if password and confirm and password != confirm:
+            raise forms.ValidationError('Password do not match')
+
+        values = {
+            "username": username,
+            "password": password
+        }
+        return values
---
+++ quora/user/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render, HttpResponse
+from .forms import RegisterForm, LoginForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate, logout
+from django.contrib import messages
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+
+            newUser = User(username=username)
+            newUser.set_password(password)
+            newUser.save()
+            login(request, newUser)
+            messages.success(request, 'Basari ile kayit oldunuz')
+            return render(request, "homepage.html")
+        context = {
+            "form": form
+        }
+        return render(request, 'register.html', context)
+    else:
+        form = RegisterForm()
+        context = {
+            "form": form
+        }
+        return render(request, 'register.html', context)
+
+
+def loginUser(request):
+    form = LoginForm(request.POST or None)
+    context = {
+        "form": form
+    }
+    if form.is_valid():
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        user = authenticate(username=username, password=password)
+
+        if user is None:
+            messages.info(request, 'Kullanici adi veya parola hatali')
+            return render(request, "login.html", context)
+        messages.success(request, 'Basari ile giris yaptiniz.')
+        login(request, user)
+        return render(request, "homepage.html")
+    return render(request, 'login.html', context)
+
+
+def logoutUser(request):
+    logout(request)
+    messages.success(request, 'Basari ile cikis yaptiniz')
+    return render(request, "homepage.html")
